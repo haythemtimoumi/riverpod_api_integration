442ffeeed1a48fb7c774f42f2bf0878ab0832e9e
Haythem Timoumi <101184653+haythemtimoumi@users.noreply.github.com>
2025-05-21 03:49:20 +0100

    Update constants.dart

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const String baseUrl = "https://ontejtest.ontej.co:8442/api/v1/";
+  static const String baseUrl = "https://xxxxxxxx:0000/api/v1/";
 
   static const String checkOpinionPollEndpoint =
       "opinionpoll/check-new-opinionpoll";
@@ -8,7 +8,7 @@ class AppConstants {
   static const String participateOpinionPollEndpoint =
       "opinionpoll/participate";
 
-  static String userId = "3043ae64-8bea-44a8-9dc9-f1a5eb901eba";
+  static String userId = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
   static String bearerToken =
-      "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJoYXl0aGVtMDdAZ21haWwuY29tIiwiaWF0IjoxNzQ0NTc0NDc4LCJleHAiOjE3NDQ2NjA4Nzh9.FQO-9NltfJ7HYFa5TM3WS4bg3X5iC-cSNKuXzlhQ50ScQO3fca2w77oO7sLTlJe3we4N7DfGsxulPxnCxSGRho827hP2PRCnrYUZByh5Z1jbCbDzqM92tLAhYdNigBFZm81nLUiv_HCiuW8arw5mm3DAwfrLZalSBPImVPPMTGCj4gPh7VvcDtkrW1QrND_OPA-mqtHUDkE1FzrJ11woDkvRnnsBk9jidy9Yhr39_JKNlYVvyJIjY3w7H6yvaNTRdy_VCrNQaH7ooi76f0efIH9dJ0-7YwiskeCa0L6Mr_LQJ1z7vJ8yj0QhNpIeC8gSkMEedqy2lsQVKi686N4q7g";
+      "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 }
